5fb62e7c5a5660b66806b926622eb2b60f61a16b
bianca <biancapalmaria@gmail.com>
2020-08-18 21:20:12 +0200

    Added printing exercise

---
+++ printing.py
@@ -0,0 +1,14 @@
+print("Winter is comming.")
+print("You know nothing" , "Bianca Maria Pal")
+
+print("If for a moment God would forget that I am a rag doll and give me a scrap of life, possibly I would not say everything that I think, but I would definitely think everything that I say.")
+
+print("I would value things not for how much they are worth but rather for what they mean.")
+
+print("I would sleep little, dream more. I know that for each minute that we close our eyes we lose sixty seconds of light.")
+
+print("I would walk when the others loiter; I would awaken when the others sleep.")
+
+print("I would listen when the others speak, and how I would enjoy a good chocolate ice cream.")
+
+print("If God would bestow on me a scrap of life, I would dress simply, I would throw myself flat under the sun, exposing not only my body but also my soul.")
