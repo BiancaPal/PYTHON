561321a09225615dc26d3adccdacee8d8e961686
bianca <biancapalmaria@gmail.com>
2020-08-19 04:17:15 +0200

    added math lesson

---
+++ math.py
@@ -0,0 +1,10 @@
+# + plus
+# - minus
+# / devide
+# * times
+# ** exponentiation
+# % modulo
+# < less than
+# > greater than 
+
+print ("What is the answer to life, the universe, and everything?", 40 + 30 - 7 * 2 / 3)
