de261fb65f23a2cf45303150c4bd3099b3da911e
bianca <biancapalmaria@gmail.com>
2020-08-19 23:17:39 +0200

    Modified my code in the tip-calculator

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,9 +1,12 @@
-total_amount = input("How much is the total amount? ")
-total_amount_int = int(total_amount.replace("$", " "))
+import os 
+os.system('clear')
 
-tip_15 = total_amount_int*0.15
-tip_18 = total_amount_int*0.18
-tip_20 = total_amount_int*0.20
+total_amount = float(input("How much is the total amount? ").strip('$'))
+
+
+tip_15 = total_amount*0.15
+tip_18 = total_amount*0.18
+tip_20 = total_amount*0.20
 print (f"A 15% tip would be an amount of ${tip_15:.2f}")
 print (f"A 18% tip would be an amount of ${tip_18:.2f}")
 print (f"A 20% tip would be an amount of ${tip_20:.2f}")
