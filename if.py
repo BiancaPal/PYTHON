a84c463fb0fe88ecd389b790668a1cbe148fbe36
bianca <biancapalmaria@gmail.com>
2020-08-20 04:48:39 +0200

    Modified the if.py file shortening the code  down with list and in

--- if.py
+++ if.py
@@ -1,9 +1,12 @@
 answer = input ("Do you want to hear a joke?")
 
-if answer == "Yes":
+affirmative_responses = ["yes", "Yes", "Y" , "y"]
+negative_responses = ["No", "no", "N", "n"]
+
+if answer in affirmative_responses:
   print("I'm against picketing, but I don't know how to show it.")
 
-elif answer == "No":
+elif answer in negative_responses:
   print("Fine")
 else:
   print("I don't understand")
