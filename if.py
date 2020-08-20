925a80138260f2f0f8c9109dc59683bde9e4a0e5
bianca <biancapalmaria@gmail.com>
2020-08-20 04:58:18 +0200

    Shortened down the code by lowercase function lower() and in

--- if.py
+++ if.py
@@ -1,12 +1,9 @@
 answer = input ("Do you want to hear a joke?")
 
-affirmative_responses = ["yes", "Yes", "Y" , "y"]
-negative_responses = ["No", "no", "N", "n"]
-
-if answer in affirmative_responses:
+if  "y" in answer.lower():
   print("I'm against picketing, but I don't know how to show it.")
 
-elif answer in negative_responses:
+elif "n" in answer.lower():
   print("Fine")
 else:
   print("I don't understand")
