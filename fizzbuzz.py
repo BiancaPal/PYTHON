999e9b8ec992c7f1e9a61769a99bd2fe248234fc
bianca <biancapalmaria@gmail.com>
2020-09-02 15:53:48 +0200

    Added exercise FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,21 @@
+# write a program that prints the numbers from 1 to 100
+# But for multiples of three print "Fizz" instead of the number
+# and for multiples of five print "BUzz".For multiples of 
+# both three and five print "FizzBuzz"
+
+# Hint: % modulo tells you what's left over when you devide one number by another
+# ex. number % 3 == 0
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0: 
+    print("Fiz")
+  elif number % 5 == 0:
+    print("Buzz")
+  
+  else:
+    print(number)
+
+  
+
