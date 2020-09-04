ce6d98f6fc67ba00c4a930e20d4129285d222650
bianca <biancapalmaria@gmail.com>
2020-09-04 16:55:37 +0200

    Added index.html file connected with python

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!!!'
-  
+def index():
+    title = 'Homepage'
+    return render_template('index.html', title=title)
+
+@app.route('/about')
+def about():
+    title = 'About'
+    return render_template('about.html',title=title)
+
+@app.route('/contact')
+def contact():
+    title = 'Contact'
+    return render_template('contact.html',title=title)
