f5c4e6ddc36c2605d5afa7e4964435a4550281b3
bianca <biancapalmaria@gmail.com>
2020-09-14 15:48:01 +0200

    Added strings with single quotes and double quotes

---
+++ INTRODUCTION TO PYTHON/strings_and_quotes.py
@@ -0,0 +1,7 @@
+phrase_1 = "This is a string"
+phrase_2 = 'This is also a string'
+
+phrase_3 = 'I told my friend, "Python is my favorite language"'
+phrase_4 = "The language 'Python' is named after Monty Python, not he snake."
+
+print(phrase_1,"\n",phrase_2,"\n",phrase_3,"\n",phrase_4)
