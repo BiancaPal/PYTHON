c9efbddb1e05b1eac3fc629697adc8204eb38fb1
bianca <biancapalmaria@gmail.com>
2020-09-14 15:26:49 +0200

    Added Hello World file, with variable names theory

--- INTRODUCTION TO PYTHON/hello_world.py
+++ INTRODUCTION TO PYTHON/hello_world.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+message = "Hello Python world!"
+
+print(message)
+
+message = "Hello  Python Crash Course World!"
+print(message)
+
+# Naming and using variables
+# -Variables names can contain only letters, numbers and underscores. They can start with a letter or an underscore but not with a number
+# -Spaces are not allowed in variable names, but underscores can be used to separate words in variable names
+# -Avoid using Python keywords and functions names as variable names, that is not to use words that Python has reserved for a particular programmatic purpose, such as the word print.
+# -Variable names should be short but descriptive.
+# -Be careful when using, "l" or "O", they can be confused with 1 and 0.
+
+# -The Python variables you're using at this point shoul be lowercase.
