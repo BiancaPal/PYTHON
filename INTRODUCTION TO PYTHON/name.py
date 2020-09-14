cec40bd7dc97a01b139f96ad87100e9c0db47cc9
bianca <biancapalmaria@gmail.com>
2020-09-14 15:58:54 +0200

    Added strings with methods title(), upper(), lower()

---
+++ INTRODUCTION TO PYTHON/name.py
@@ -0,0 +1,7 @@
+name = "ada lovelace"
+# this method changes each word to title case, where each word begins with a capital letter.
+print(name.title())
+# this method changes a string to uppercase 
+print(name.upper())
+# this method changes a string to lowercase
+print(name.lower())
