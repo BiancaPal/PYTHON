6728369bf4638546359e74231cf579b56d5ab950
bianca <biancapalmaria@gmail.com>
2020-09-15 16:24:36 +0200

    Added mantra of python

---
+++ INTRODUCTION TO PYTHON/Mantra_of_python.py
@@ -0,0 +1,9 @@
+# Anything following a hash mark in your code is ignored by the Python interpreter
+
+# Say hello to everyone
+print("Hello everyone!")
+
+# The main reason to write comments is to explain what code is supposed to do and how you are making it work. After some time apart, you can always study your code for a while and figure out how segments were supposed to work.
+# In order to become a better programmer or collaborate with other programmers, you should write meaningful comments. Writing clear, concise comments in yout code is one of the most beneficial habits you can form as a new programmer.
+# When you're determining whether to write a comment, ask yourself is you had to consider several approaches before coming up with a reasonable way to make something work, if so write a comment about your solution.
+
