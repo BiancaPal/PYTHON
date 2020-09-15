d044dc5629791ede708d2945dd846ca68eec2b06
bianca <biancapalmaria@gmail.com>
2020-09-15 15:23:41 +0200

    Added some try out yourself exercise

---
+++ INTRODUCTION TO PYTHON/try_yourself_stripping_exercise.py
@@ -0,0 +1,11 @@
+name = "Eric"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+quote = " A person who never made a mistake never tried anything new"
+famous_person = 'Albert Einstein'
+
+print(f"\n\t{famous_person} once said,\n\t {quote}")
