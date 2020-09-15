6a419ecc68400435d96e9f99ac3775b06d1cf2e8
bianca <biancapalmaria@gmail.com>
2020-09-15 21:32:42 +0200

    Added try yourself exercise with lists

---
+++ INTRODUCTION TO PYTHON/try_yourself_lists.py
@@ -0,0 +1,9 @@
+names = ['Natalie Portman', 'Jesus Christ', 'Jason Momoa', 'Judas', 'Manolito']
+
+for name in range(0,5):
+  print(f"I would like to meet with {names[name]}")
+
+vehicles = ['honda motorcycle', 'plane', 'roulotte']
+
+for vehicle in range(0,3):
+  print(f"I would like to own a {vehicles[vehicle]}")
