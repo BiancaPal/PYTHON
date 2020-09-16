1821f11f675b6ca728be7fa9e28e0308082e78cb
bianca <biancapalmaria@gmail.com>
2020-09-16 16:41:47 +0200

    Added try it yourself exercise with loops

---
+++ INTRODUCTION TO PYTHON/try_yourself_array_loop.py
@@ -0,0 +1,13 @@
+pizzas = ['quattro stagioni', 'quoattro formaggi', 'diavola', 'romana', 'prosciutto e funghi', 'capricciosa', 'calzone']
+
+for pizza in pizzas:
+  print(f"I would like to order a {pizza}")
+
+print("I love pizza")
+
+animals = ['kitten', 'parrot', 'dog', 'hamster']
+
+for animal in animals:
+  print(f"A {animal} would make a great pet")
+
+print("Any of these animals would make a great pet")
