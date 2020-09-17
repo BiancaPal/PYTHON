76150c4d9dc5d142b097164ecb8f2e7c4558b31d
bianca <biancapalmaria@gmail.com>
2020-09-18 00:24:14 +0200

    Added try yourself tuple exercise

---
+++ INTRODUCTION TO PYTHON/try_yourself_tuple.py
@@ -0,0 +1,9 @@
+foods = ('fish and chips', 'salad', 'burguer', 'pizza', 'pasta')
+
+for food in foods:
+  print(food)
+
+# REASSIGN A TUPLE
+foods = ('kebab','vegetable curry with rice', 'cheese and tomato pizza', 'meatballs, spaghetti and tomato sauce', 'fruit salad' )
+for food in foods:
+  print(food)
