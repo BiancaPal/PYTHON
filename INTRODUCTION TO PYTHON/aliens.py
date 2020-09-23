10bec71e351d2b793ad8a10b623b5455f0c9cb52
bianca <biancapalmaria@gmail.com>
2020-09-23 13:59:17 +0200

    Added lists inside of dictionaries

--- INTRODUCTION TO PYTHON/aliens.py
+++ INTRODUCTION TO PYTHON/aliens.py
@@ -67,3 +67,20 @@ for alien in aliens[:3]:
     alien['speed'] = 'fast'
     alien['points'] = '15'
 
+# A dictionary of lists
+# In the following example, two kinds of information are stored for each pizza: a type of 
+# crust and a list of toppings. The list of toppings is a value associated with the key
+# 'toppings'. To use the items in the list, we give the name of the dictionary and the key
+# 'toppings', as we would any value in the dictionary. Instead of returning a single value,
+# we get a list of toppings.
+
+pizza =  {
+  'crust': 'thick',
+  'toppings': ['mushrooms', 'extra cheese']
+  }
+
+# Summarize the order:
+print(f"You ordered a {pizza['crust']}-crust pizza"
+      "with the following toppings:")
+for topping in pizza['toppings']:
+  print("\t" + topping)
