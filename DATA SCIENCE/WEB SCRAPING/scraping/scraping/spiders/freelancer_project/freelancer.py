edec882794d2f0eaf7698a9644c785508c17637f
bianca <biancapalmaria@gmail.com>
2020-10-07 16:45:16 +0200

    Added scraping projects

---
+++ DATA SCIENCE/WEB SCRAPING/scraping/scraping/spiders/freelancer_project/freelancer.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class FreelancerSpider(scrapy.Spider):
+    name = 'freelancer'
+    allowed_domains = ['freelancer.com']
+    start_urls = ['https://www.freelancer.com/jobs/']
+
+    def parse(self, response):
+        self.log('I just visited: '+ response.url)
+        for service in response.css('div.JobSearchCard-item'):
+            item =  {
+            'name_job': service.css('a.JobSearchCard-primary-heading-link::text').extract(),
+            'price': service.css(' div.JobSearchCard-secondary-price::text').extract(),
+            'bids': service.css(' div.JobSearchCard-secondary-entry::text').extract(),
+            }
+            yield item
+
+        # follow pagination link
+        next_page_url = response.css('li.Pagination-item > a::attr(href)').extract()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
